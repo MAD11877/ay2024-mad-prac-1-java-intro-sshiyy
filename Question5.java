3fc1cea591c0da92123c624103d16100920fcbd4
sshiyy <148183319+sshiyy@users.noreply.github.com>
2024-04-15 15:22:39 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -6,12 +6,12 @@ public class Question5
   {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
+        
         int count = in.nextInt();
 
         int[] numbers = new int[count];
         for (int i = 0; i < count; i++) {
-            System.out.print("> ");
+            
             numbers[i] = in.nextInt();
         }
 
@@ -29,7 +29,7 @@ public class Question5
                 mode = numbers[i];
             }
         }
-        System.out.println("Mode: " + mode);
+        System.out.println(mode);
   }
 }
 
